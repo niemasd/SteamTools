9cd146075c1c697467428d7e0c9843a2307df086
Niema Moshiri <niemamoshiri@gmail.com>
2021-11-09 17:52:09 -0800

    Update SteamLibrary.py

--- SteamLibrary.py
+++ SteamLibrary.py
@@ -7,4 +7,14 @@ SteamLibrary (Niema Moshiri 2021)
 from prompt_toolkit.shortcuts import message_dialog
 
 # useful constants
+WINDOW_TITLE = "SteamLibrary - Niema Moshiri"
 STEAM_COMMUNITY_BASE_URL = "https://steamcommunity.com/id"
+
+# welcome message
+def welcome_message():
+  return message_dialog(title=WINDOW_TITLE, text="hola")
+
+# main content
+if __name__ == "__main__":
+  # show welcome message
+  welcome_message()
