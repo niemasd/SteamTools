68ea35205168c0accafe85279b22f6b20d495bcf
Niema Moshiri <niemamoshiri@gmail.com>
2021-11-09 23:50:25 -0800

    Updates

--- SteamLibrary.py
+++ SteamLibrary.py
@@ -57,56 +57,36 @@ class Game:
             error_app("%s: %s" % (ERROR_INVALID_GAME, str(game)))
         self.name = data['name']
         self.appID = data['appID']
+
+    # view game app
+    def view_app(self):
         try:
             details = jloads(urlopen("%s%s" % (STEAM_APP_DETAILS_BASE_URL, self.appID)).read().decode())[self.appID]['data']
         except:
             details = dict()
-        if 'release_date' in details and 'date' in details['release_date']:
-            self.release_date = details['release_date']['date']
-        if 'developers' in details:
-            self.developers = details['developers']
-        if 'publishers' in details:
-            self.publishers = details['publishers']
-        if 'genres' in details:
-            self.genres = sorted(g['description'] for g in details['genres'])
-        if 'categories' in details:
-            self.categories = sorted(c['description'] for c in details['categories'])
-        #if 'controller_support' in details:
-        #    self.controller_support = details['controller_support']
-        if 'supported_languages' in details:
-            self.supported_languages = details['supported_languages']
-        if 'release_date' in details and 'date' in details['release_date']:
-            self.release_date = details['release_date']['date']
-        if 'short_description' in details:
-            self.short_description = details['short_description']
-        if 'price_overview' in details and 'final_formatted' in details['price_overview']:
-            self.price = details['price_overview']['final_formatted']
-        if 'achievements' in details and 'total' in details['achievements']:
-            self.num_achievements = details['achievements']['total']
-
-    # view game app
-    def view_app(self):
         text = ''
         #text += '<ansired>- Name: </ansired> %s\n' % self.name
         text += '<ansired>- App ID:</ansired> %s\n' % self.appID
-        if hasattr(self, 'release_date'):
-            text += '<ansired>- Release Date:</ansired> %s\n' % self.release_date
-        if hasattr(self, 'developers'):
-            text += '<ansired>- Developers:</ansired> %s\n' % ', '.join(self.developers)
-        if hasattr(self, 'publishers'):
-            text += '<ansired>- Publishers:</ansired> %s\n' % ', '.join(self.publishers)
-        if hasattr(self, 'price'):
-            text += '<ansired>- Price:</ansired> %s\n' % self.price
-        if hasattr(self, 'num_achievements'):
-            text += '<ansired>- Achievements:</ansired> %s\n' % self.num_achievements
-        if hasattr(self, 'genres'):
-            text += '<ansired>- Genres:</ansired> %s\n' % ', '.join(self.genres)
-        if hasattr(self, 'categories'):
-            text += '<ansired>- Categories:</ansired> %s\n' % ', '.join(self.categories)
-        #if hasattr(self, 'controller_support'):
-        #    text += '<ansired>- Controller Support:</ansired> %s\n' % self.controller_support
-        if hasattr(self, 'supported_languages'):
-            text += '<ansired>- Supported Languages:</ansired> %s\n' % self.supported_languages.replace('<strong>','').replace('</strong>','').replace('<br>','; ')
+        if 'release_date' in details and 'date' in details['release_date']:
+            text += '<ansired>- Release Date:</ansired> %s\n' % details['release_date']['date']
+        if 'developers' in details:
+            text += '<ansired>- Developers:</ansired> %s\n' % ', '.join(details['developers'])
+        if 'publishers' in details:
+            text += '<ansired>- Publishers:</ansired> %s\n' % ', '.join(details['publishers'])
+        if 'price_overview' in details and 'final_formatted' in details['price_overview']:
+            text += '<ansired>- Price:</ansired> %s\n' % details['price_overview']['final_formatted']
+        if 'achievements' in details and 'total' in details['achievements']:
+            text += '<ansired>- Achievements:</ansired> %s\n' % details['achievements']['total']
+        if 'genres' in details:
+            text += '<ansired>- Genres:</ansired> %s\n' % ', '.join(sorted(g['description'] for g in details['genres']))
+        if 'categories' in details:
+            text += '<ansired>- Categories:</ansired> %s\n' % ', '.join(sorted(c['description'] for c in details['categories']))
+        #if 'controller_support' in details:
+        #    text += '<ansired>- Controller Support:</ansired> %s\n' % details['controller_support']
+        if 'supported_languages' in details:
+            text += '<ansired>- Supported Languages:</ansired> %s\n' % details['supported_languages'].replace('<strong>','').replace('</strong>','').replace('<br>','; ')
+        #if 'short_description' in details:
+        #    text += '<ansired>- Description:</ansired> %s\n' % details['short_description']
         message_dialog(title=self.name, text=HTML(text)).run()
 
     # str function
@@ -146,11 +126,8 @@ if __name__ == "__main__":
         error_app(ERROR_LOAD_GAMES_FAILED)
 
     # load game data
-    games_list = list()
-    for i, xml_game in enumerate(xml_games):
-        message(s="Loading game %d of %d..." % (i+1, len(xml_games)), end='\r')
-        games_list.append(Game(xml_game))
-    games_list.sort()
+    games_list = sorted(Game(xml_game) for xml_game in xml_games)
+    games_map = {game.appID:game for game in games_list}
 
     # view games (might need to move this into its own function if I want to add a filtering option)
     game_list_dialog = radiolist_dialog(title=WINDOW_TITLE, text="HELLO", values=[(game,game.name) for game in games_list])
