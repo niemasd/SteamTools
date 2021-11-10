d3fb39d295a578dccccfa16f8d7b7b5f62203cf9
Niema Moshiri <niemamoshiri@gmail.com>
2021-11-09 17:48:05 -0800

    Create SteamLibrary.py

---
+++ SteamLibrary.py
@@ -0,0 +1,10 @@
+#! /usr/bin/env python3
+'''
+SteamLibrary (Niema Moshiri 2021)
+'''
+
+# imports
+from prompt_toolkit.shortcuts import message_dialog
+
+# useful constants
+STEAM_COMMUNITY_BASE_URL = "https://steamcommunity.com/id"
