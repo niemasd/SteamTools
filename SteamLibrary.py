49bc9170bff0c6234987667e4fd0306e35821978
Niema Moshiri <niemamoshiri@gmail.com>
2021-11-10 02:01:00 +0000

    Update

--- SteamLibrary.py
+++ SteamLibrary.py
@@ -3,12 +3,23 @@
 SteamLibrary (Niema Moshiri 2021)
 '''
 
-# imports
-from prompt_toolkit.shortcuts import message_dialog
-
 # useful constants
 WINDOW_TITLE = "SteamLibrary - Niema Moshiri"
 STEAM_COMMUNITY_BASE_URL = "https://steamcommunity.com/id"
+ERROR_IMPORT_PROMPT_TOOLKIT = "Unable to import 'prompt_toolkit'. Install via: 'pip install prompt_toolkit'"
+
+# built-in imports
+from sys import stderr
+
+# error message
+def error(s):
+    print("ERROR: %s" % s, file=stderr); exit(1)
+
+# non-built-in imports
+try:
+    from prompt_toolkit.shortcuts import message_dialog
+except:
+    error(ERROR_IMPORT_PROMPT_TOOLKIT)
 
 # apps
 APPS = {
